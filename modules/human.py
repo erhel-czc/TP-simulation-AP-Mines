ad49a07209c96d2496255661a923839cf6951c1f
Erhel czc <erhelito@yahoo.fr>
2026-01-20 16:58:42 +0100

    empty modyles

---
+++ modules/human.py
@@ -0,0 +1,6 @@
+class Human:
+    def __init__(self, init_x: int, init_y: int, size: int, max_speed: int) -> None:
+        self.x = init_x
+        self.y = init_y
+        self.size = size
+        self.max_speed = max_speed
---
+++ modules/solid.py
@@ -0,0 +1,13 @@
+class Solid:
+    def __init__(self, x1: int, y1: int, x2: int | None, y2: int | None, radius: int | None) -> None:
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.radius = radius
+
+    def cercle(self):
+        pass
+
+    def rectangle(self):
+        pass
