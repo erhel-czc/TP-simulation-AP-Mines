96cde2183dac4e1e48d0ea8a66ed4bd3f7e88fac
Erhel czc <erhelito@yahoo.fr>
2026-01-21 15:00:21 +0100

    Implement initial Window class for arcade simulation

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -1,3 +1,20 @@
+import arcade
+
+class Window(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.ASH_GREY)
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        self.clear()
+        pass
+
+    def update(self, delta_time):
+        pass
+
 def main():
     pass
 
